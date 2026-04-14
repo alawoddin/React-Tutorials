cdc82cacd46601429176eddeaf25ab52cc4aa17b
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-04-14 10:42:12 +0430

    the use effect is done

--- src/Components/State/Effect.jsx
+++ src/Components/State/Effect.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 
 const Effect = () => {
-    useEffect(() => {
-        console.log("Component Loaded");
-    } , []);
-    return (
-        <div>
-            <h1>hello</h1>
-        </div>
-    );
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://dummyjson.com/products/1")
+      .then((res) => res.json())
+      .then(data => setData(data));
+  } , []);
+  return (
+    <div>
+      <h1>name :{data.title}</h1>
+      <p>des {data.description}</p>
+    </div>
+  );
 };
 
 export default Effect;
